8b744fdfcbb884db7d3c23e83e97c8a831158e41
muriloVSantos <murilo2santos03@gmail.com>
2022-05-27 21:04:30 +0000

    Atualiza para uma aplicação web usando Flask

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print('Olá mundo')
+# Ao abrir o Gitpod execute:
+# pip install -r requirements.txt
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World'
+
+app.run()
